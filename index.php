726d552787e537918eb4b67fec81c1d703238d45
talibehackeur <hackeurtalibe@gmail.com>
2016-11-08 23:30:17 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require "vendor/autoload.php";
+$mollie = new Mollie_API_Client();
+$mollie->setApiKey("test_ueH25gaBac9NcjqSNmjeg6CvzztDUE");
+$payment = $mollie->payments->create(array(
+    "amount" => 10.00,
+    "description" => "My first API payment",
+    "redirectUrl" => "https://webshop.example.org/order/12345/",
+));
+$payment = $mollie->payments->get($payment->id);
+if ($payment->isPaid()) {
+    die ("Payment received.");
+} else {
+    die ("Payment cancelled.");
+}
