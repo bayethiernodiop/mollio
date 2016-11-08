c6b91f7ffdae317947e3799483007e0ac9d05fbf
talibehackeur <hackeurtalibe@gmail.com>
2016-11-08 23:51:47 +0000

    first commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $mollie->setApiKey("test_ueH25gaBac9NcjqSNmjeg6CvzztDUE");
 $payment = $mollie->payments->create(array(
     "amount" => 10.00,
     "description" => "My first API payment",
-    "redirectUrl" => "https://webshop.example.org/order/12345/",
+    "redirectUrl" => "https://molio.herokuapp.com",
 ));
 $payment = $mollie->payments->get($payment->id);
 if ($payment->isPaid()) {
