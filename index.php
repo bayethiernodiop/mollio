5e0bcce3ab4ee596c3bd8e468cf7e8ce9ca04768
talibehackeur <hackeurtalibe@gmail.com>
2016-11-09 00:31:03 +0000

    first commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $mollie->setApiKey("test_ueH25gaBac9NcjqSNmjeg6CvzztDUE");
 $payment = $mollie->payments->create(array(
     "amount" => 10.00,
     "description" => "My first API payment",
-    "redirectUrl" => "https://molio.herokuapp.com/payment-result.php",
+    "redirectUrl" => "https://molio.herokuapp.com/redirect-after-payment.php",
     "webhookUrl" => "https://molio.herokuapp.com/webhook.php"
 ));
 $payment = $mollie->payments->get($payment->id);
